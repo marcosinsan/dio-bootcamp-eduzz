79d316f5689a3d189ab4a96a8fa7e5904ad7624b
Marco Santana <marcos.sinsan@gmail.com>
2021-11-25 10:25:36 -0300

    Alterando arquivo index.ts

--- typescript/index.ts
+++ typescript/index.ts
@@ -2,4 +2,45 @@ function soma (a: number, b: number) {
     return a + b;
 }
 
-soma(10, 5);
+
+// interfaces
+
+interface IAnimal {
+    nome: String;
+    tipo: 'terrestre' | 'aquático';
+    domestico: boolean;
+   
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+interface ICanino extends IAnimal {
+    porte: 'pequeno' | 'médio' | 'grande';
+}
+
+const animal: IAnimal = {
+    nome: "Elefante",
+    tipo: 'terrestre',
+    domestico: false
+}
+
+// types
+type IDomestico = ICanino | IFelino;
+
+const animal2: IDomestico = {
+    domestico: true,
+    nome: 'cachorro',
+    tipo: 'terrestre',
+    porte: 'médio',
+    visaoNoturna: false
+}
+
+const felino: IDomestico = {
+    domestico: false,
+    nome: "Leão",
+    tipo: 'terrestre',
+    visaoNoturna: true
+}
+
